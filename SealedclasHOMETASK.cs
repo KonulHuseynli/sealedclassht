23ae9da4d54cf4440ba5a226c7dd03f594aa5ee0
KonulHuseynli <konulrh@code.edu.az>
2022-06-10 12:03:56 +0400

    addedoldtask

---
+++ SealedclasHOMETASK.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tasksealed(27);
+            Tasksealed("Konul");
+            Tasksealed('h');
+            Tasksealed(89.1);
+            
+        }
+        public static void Tasksealed(object item)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
